8d3c7b39e00acbf0b69a349bc289de65f12bfc1e
3amoBadawy <79743061+3amoBadawy@users.noreply.github.com>
2025-08-10 01:40:17 +0000

    chore: bump BUILD_ID + add apiFetch log

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,3 +1,5 @@
+import { BUILD_ID } from './build-id.js';
+console.log('HF Build ID:', BUILD_ID);
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
